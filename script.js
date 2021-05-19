b5c183ce0d9b3e5b4f40129e1b7744124ccc7ca3
Bipradeep Saha <bs19ms135@iiserkol.ac.in>
2021-05-20 00:36:59 +0530

    Fixed menu toggle button. Added Logo to navbar. Added Homepage Layout.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navbar = document.getElementsByClassName('navbar')[0]
+const quote = document.getElementsByClassName('welcome-quote')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
+    navbar.classList.toggle('active')
+    quote.classList.toggle('active')
 }) 
