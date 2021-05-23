512ce59f1439e0821bd274c9e7a717a9ef87b133
Bipradeep Saha <bs19ms135@iiserkol.ac.in>
2021-05-23 19:57:41 +0530

    Added local Storage use to dark mode, but not working on rest. Fixed padding issue on currently working page.

--- script.js
+++ script.js
@@ -11,12 +11,30 @@ toggleButton.addEventListener('click', () => {
 
 // dark mode toggle
 var icon = document.getElementById("icon")
+
+if(localStorage.getItem("theme") == null){
+    localStorage.setItem("theme", "light")
+}
+
+
+let localData = localStorage.getItem("theme")
+if(localData == "light"){
+    icon.src = "/media/icons/moon.png"
+    document.body.classList.remove("dark-theme")
+}
+else if(localData == "dark"){
+    icon.src = "/media/icons/sun.png"
+    document.body.classList.add("dark-theme")
+}
+
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         icon.src = "/media/icons/sun.png"
+        localStorage.setItem("theme", "dark")
     }
     else {
         icon.src = "/media/icons/moon.png"
+        localStorage.setItem("theme", "light")
     }
 }
