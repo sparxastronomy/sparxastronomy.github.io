04ab425ff79721a4f8d424bd8d6b61cba4ba9f50
Bipradeep Saha <bs19ms135@iiserkol.ac.in>
2022-12-30 00:22:37 +0530

    First Commit with basic layout and configuration.

---
+++ src/config/nav.js
@@ -0,0 +1,18 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'Research',
+		slug: '/blog',
+	},
+	{
+		title: 'About',
+		slug: '/company/about',
+	},
+	{
+		title: 'Contact',
+		slug: '/company/contact',
+	},
+];
---
+++ tailwind.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue,css}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: colors.blue,
+				secondary: colors.pink,
+				'trust': {
+					50: '#eceff1',
+					400: '#90a4ae',
+					500: '#607d8b',
+					800: '#37474f',
+					900: '#263238',
+				},
+				'dark-b': '#111827',
+				'midnight': '#121063',
+			},
+			fontFamily: {
+				sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
+				//add varela round
+				'body': ["'Varela Round'", ...defaultTheme.fontFamily.sans],
+			},
+			},
+			variants: {
+			  backgroundColor: ['responsive', 'hover', 'focus', 'dark'],
+			  textColor: ['responsive', 'hover', 'focus', 'dark',],
+			  borderColor: ['responsive', 'hover', 'focus', 'dark'],
+			  animation: ['hover', 'focus']
+			},
+	},
+	plugins: [require('@tailwindcss/typography')],
+	darkMode: 'class',
+};
