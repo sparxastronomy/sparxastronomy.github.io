a498de715cdd24354ec322c5c90c12474344ca13
Bipradeep Saha <bs19ms135@iiserkol.ac.in>
2022-12-30 10:35:24 +0530

    Fixed math rendering equaiton

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,30 @@ import image from "@astrojs/image";
 import lit from "@astrojs/lit";
 import tailwind from "@astrojs/tailwind";
 
+// remark math and rehype katex
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  
   site: 'https://sparxastronomy.github.io/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit(), tailwind()],
-  // Add renderers to the config
-  // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
-  vite: {
+
+  integrations: [sitemap(), image(), lit(), tailwind(), mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  })],
+
+   vite: {
     ssr: {
       external: ["svgo"]
     }
   },
   output: "static",
-  adapter: netlify()
+  // adapter: netlify(),
 });
