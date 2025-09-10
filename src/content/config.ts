b188a2f63a3663783ca19936ca91f2406cdc5404
Bipradeep Saha <bipradeepsaha04@gmail.com>
2025-09-11 01:22:42 +0530

    feat: add blog functionality with new posts and update navbar links

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,17 +14,5 @@ export const collections = {
 			excerpt: z.string().optional(),
 		}),
 	}),
-	work: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			featuredImage: z.string(),
-			img_alt: z.string().optional(),
-			excerpt: z.string().optional(),
-		}),
-	}),
 };
 
